8380fba3430e816e8f599703773dced989223c5f
Yuriy Gritsienko <yuriy.gritsienko1@gmail.com>
2024-01-17 23:30:58 +0300

    Added homework #2

---
+++ homework_lecture_2/homework_lecture_2.rb
@@ -0,0 +1,12 @@
+def greeting(first_name, last_name, age)
+  full_name = "#{first_name} #{last_name}"
+  if age < 18
+    "Привет #{full_name}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано."
+  else
+    "Привет #{full_name}. Самое время заняться делом!"
+  end
+end
+
+def foobar(foo, bar)
+  (foo == 20 || bar == 20) ? bar : foo + bar
+end
